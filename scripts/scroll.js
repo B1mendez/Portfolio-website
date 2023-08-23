ff61e4fc382ca5dc4163b44f4c21da01bf198aea
Brian Mendez <brianmendez2308@gmail.com>
2023-08-23 13:46:24 -0700

    alignment and spacing complete

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            console.log(entry)
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
             }
