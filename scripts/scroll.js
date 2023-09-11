2e648d699b9b59371fca1b0de760bc4fc5ab7d47
B1mendez <Brianmendez2308@gmail.com>
2023-09-10 21:55:02 -0700

    contact page and added projects

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -10,11 +10,35 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     },
     {
-      threshold: 0.25,
+      threshold: 0.15,
     }
   );
 
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
+
+
+  // var leftHeading = document.getElementById('contact-heading-left');
+  // var rightHeading = document.getElementById('contact-heading-right');
+
+  // window.addEventListener('scroll', function () {
+  //   var s = window.scrollY,
+  //     d = document.documentElement.scrollHeight,
+  //     c = window.innerHeight;
+
+  //   var scrollPercent = (s*2) / (d - c);
+
+  //   // Calculate translation based on scroll percentage
+  //   // You can adjust the multiplier for a stronger/weaker effect
+  //   var leftTranslation = scrollPercent * -100;   // Negative to move to the left
+  //   var rightTranslation = scrollPercent * 100;  // Positive to move to the right
+
+  //   console.log(leftTranslation);
+  //   console.log(rightTranslation);
+  //   // Update the transform property
+  //   leftHeading.style.transform = `translateX(${Math.min(leftTranslation, -86)}%)`;
+  //   rightHeading.style.transform = `translateX(${Math.max(rightTranslation, 86)}%)`;
+    
+  // });
 });
 
